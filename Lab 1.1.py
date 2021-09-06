d5f5b450f458e520b7c810d2ac4f39009ededc03
TXY1282 <90165924+TXY1282@users.noreply.github.com>
2021-09-06 10:37:59 +0800

    Add files via upload

---
+++ Lab 1.1.py
@@ -0,0 +1,9 @@
+# Student ID:1201201282#
+# Student Name:Tay #
+
+print("I like Python")
+
+person = input("Please Enter Your Name: ")
+print("Hello ",person)
+
+# Lab 1.2 get input two numbers and display the sum of the two numbers #
